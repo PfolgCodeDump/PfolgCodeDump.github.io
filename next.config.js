badd492660d33c54a94cf3ddc9fde37d6a78d1bf
csy214-beep <csy2146047622@gmail.com>
2026-04-14 23:03:03 +0800

    fix: url path

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
   output: "export", // 开启纯静态导出
   trailingSlash: true, // 路径统一带/，避免路由异常
   distDir: "output",
+  basePath: "/Pblog", // 替换成你的仓库名
+  assetPrefix: "/Pblog/", // 注意末尾的斜杠
 };
 
 module.exports = nextConfig;
