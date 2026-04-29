0b128269d5611c69d246c86fe010b7ccd2d5caa6
csy214-beep <csy214-beep@users.noreply.github.com>
2026-04-29 05:05:54 +0000

    deploy: 558b1f851fdb620c05954420fabbe7c06a295b03

---
+++ _next/static/MZhIdgmGY1_zgDL1s_dQX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/FunRadiusP//_next/:path+",
+        "destination": "/FunRadiusP/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ demos/demo2/assets/script.js
@@ -0,0 +1,11 @@
+function showImage(src) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "flex";
+  lightboxImg.src = src;
+}
+
+function closeLightbox() {
+  const lightbox = document.getElementById("lightbox");
+  lightbox.style.display = "none";
+}
